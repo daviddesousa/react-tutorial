b7370b8348707edd576e4d95888e876c06ac9ae4
David De Sousa <david@littlebox.io>
2019-10-20 19:43:05 -0400

    authentication flow in Redux

---
+++ src/utils/authcheck.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class AuthCheck extends Component {
+  render() {
+    return (
+        <div>
+        </div>
+    );
+  }
+}
+
+function mapStateToProps(dispatch) {
+  return {
+
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthCheck);
