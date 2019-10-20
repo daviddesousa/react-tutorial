86f7e5058273db5b8c6522807e7af46703996101
David De Sousa <david@littlebox.io>
2019-10-20 19:28:03 -0400

    handling the callback and authentication

---
+++ src/functional/callback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Callback = props => (
+    <div>
+      Callback
+      {console.log(props)}
+      {/*Typically a loading screen...*/}
+    </div>
+);
+
+export default Callback;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
 import Component1 from './functional/component1';
+import Callback from './functional/callback';
 
 import Container1 from './containers/container1';
 import Header from './containers/header';
 import history from './utils/history';
+import Auth from './utils/auth';
 
 import {Router, Route, Switch} from 'react-router';
+
+const auth = new Auth();
+
+const handleAuthentication = (props) => {
+  if (props.location.hash) {
+    auth.handleAuth();
+  }
+};
 
 class Routes extends Component {
   render() {
@@ -16,7 +26,13 @@ class Routes extends Component {
             <div>
               <Header/>
               <Switch>
-                <Route exact path="/" component={Container1}/>
+                <Route exact path="/"
+                       render={() => <Container1 auth={auth}/>}/>
+                <Route path="/callback"
+                       render={(props) => {
+                         handleAuthentication(props);
+                         return <Callback/>;
+                       }}/>
                 <Route path="/component/:id"
                        render={(props) => <Component1 {...props}/>}/>
               </Switch>
